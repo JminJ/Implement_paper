4d33c0705f317fec7f042c0b23ab267a0755b985
Jeoung-Minju <jminju254@gmail.com>
2021-05-08 17:40:49 +0900

    add Encoder_Layer

--- bert/BERT.py
+++ bert/BERT.py
@@ -1,41 +1,6 @@
 import torch
 import torch.nn as nn
 import numpy as np
-
-class Embedding(nn.Module):
-    def __init__(self, input, embedding_size):
-        super(Embedding, self).__init__()
-        # input은 bpe 알고리즘으로 전처리 및 padding 되어있을 것입니다.
-        self.embedding_size = embedding_size # gpt-1 : 768
-
-        self.embedding = nn.Embedding(self.input_len[-1], self.embedding_size) # self.input_len[-1] = (bs, seq, vocab_size)에서 vocab_size
-
-    ### positional encodding~
-    def cal_positional_encodding(self, position_now, i): # 현재 position과 i를 사용해 pos_encodding을 연산
-        pos_encodding = position_now / torch.pow(10000, (2*(i//2))/self.embedding_size)
-        return pos_encodding
-
-    def positional_encodding(self, position_now): # 현재 position 값과 embedding_size(768)를 사용해 cal_positoinal_encodding을 호출
-        return [cal_positional_encodding(powition_now, i) for i in range(self.embedding_size)]
-
-    def get_sinusoiding_table(self, input_len): # 연산된 pos_encodding에 i값에 따라 sin, cos을 적용
-        sinusoid_table = np.array([positional_encodding(i_seq) for i_seq in range(input_len[1])])
-        sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])
-        sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])
-
-        return sinusoid_table # 이것이 positional encodding 결과값
-    ### ~positional encodding
-
-    ### embedding~
-    def forward(self, input):
-        # input을 forward에서 받아오고 input_len을 get_sinusoding_table 함수로 보냄
-        input_len = input.size()
-        embedding_input = self.embedding(input) / self.embedding_size**0.5 # embedding_size(d_model)의 루트로 나눠준다.
-        pos_embedding = get_sinusoiding_table(input_len)
-
-        return embedding_input + pos_embedding
-    ### ~embedding
-        
         
 class self_dot_attention(nn.Module):
     def __init__(self, Q, K, V): # multi-head-attention에서 나눈 Q, K, V
@@ -54,7 +19,7 @@ class self_dot_attention(nn.Module):
         soft_mat = self.softmax(matmul, dim=-1)
         mul_v = torch.matmul(soft_mat, self.V)
 
-        return mul_v
+        return mul_v, soft_mat
     
 
 class multi_head_attention(nn.Module):
@@ -79,22 +44,23 @@ class multi_head_attention(nn.Module):
         W_K = self.W_K(self.K).view(self.batch_size, -1, self.n_head, self.d_head).transpose(1,2)
         W_V = self.W_V(self.V).view(self.batch_size, -1, self.n_head, self.d_head).transpose(1,2)
 
-        self_attention = self_dot_attention(W_Q, W_K, W_V)
+        self_attention, soft_mat = self_dot_attention(W_Q, W_K, W_V)
         print(self_attention.size())
 
         concat_attentions = self_attention.contiguous().view(self.batch_size, -1, self.d_model)
 
         output = self.last_linear(concat_attentions)
 
-        return output
+        return output, soft_mat
 
-class Encoder(n.Module):
-    def __init__(self, inputs, n_head, batch_size, d_model):
-        super(Encoder, self).__init__()
+class Encoder_Layer(nn.Module):
+    def __init__(self, inputs, n_head, batch_size, d_model, seq_len_size):
+        super(Encoder_Layer, self).__init__()
         self.inputs = inputs
         self.n_head = n_head
         self.batch_size = batch_size
-        self.d_model
+        self.d_model = d_model
+        self.seq_len_size = seq_len_size
 
         self.m_Q = nn.Linear(self.inputs.size(-1), d_model)
         self.m_K = nn.Linear(self.inputs.size(-1), d_model)
@@ -105,6 +71,11 @@ class Encoder(n.Module):
         self.ffnn_linear1 = nn.Linear(d_model * 4, d_model) # transformer에서 * 4를 취해주므로 본 코드에서도 따라갑니다.
         self.ffnn_gelu = nn.GeLU() # ReLU 대신 GeLU를 사용합니다.
         self.ffnn_linear2 = nn.Linear(d_model * 4, d_model) # transformer에서 * 4를 취해주므로 본 코드에서도 따라갑니다.
+
+    def first_hidden_state(self):
+        initial_hidden = torch.random(self.batch_size, self.seq_len_size, self.d_model)
+
+        return initial_hidden
 
     def add_and_norm(self, before_result, now_result):
         add_result = before_result + now_result
@@ -119,8 +90,43 @@ class Encoder(n.Module):
 
         return linear2
 
+    def forward(self, Q, K, V):
+        before_result = self.first_hidden_state()
+        attn_result, soft_mat = multi_head_attention(Q, K, V, self.n_head, self.batch_size, self.d_model)
+
+        a_n_result = self.add_and_norm(before_result, attn_result)
+        
+        feed_result = self.feed_forward_function(a_n_result)
+
+        return feed_result, soft_mat
+
+class Encoder(nn.Module):
+    def __init__(self, inputs, n_head, batch_size, d_model, seq_len_size):
+        super(Encoder, self).__init__()
+        self.inputs = inputs
+        self.n_head = n_head
+        self.batch_size = batch_size
+        self.d_model = d_model
+        self.seq_len_size = seq_len_size
+
+        self.encoder_layers = Encoder_Layer(inputs, n_head, batch_size, d_model, seq_len_size)
+        
     def forward(self):
+        soft_mats = []
+
+        result, soft_mat = self.encoder_layers(self.inputs)
+        for _ in range(self.n_head-1):
+            result, soft_mat = self.encoder_layers(result)
+            soft_mats.append(soft_mat)
+        return result, soft_mats
+
+class BERT(nn.Module):
+    def __init__(self):
+        super(BERT, self).__init__()
         pass
+
+
+        
 
     
 
